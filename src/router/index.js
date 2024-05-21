9e31985769837b0ff0b1d55a9908229174149a19
xiao.wenpeng <ja_xwp@yeah.net>
2024-05-21 12:13:35 +0900

    6.6 制作Vue3公共组件

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import {createRouter,createWebHistory} from 'vue-router'
+
+import Home from '../view/Home.vue'
+import Login from '../view/Login.vue'
+
+const routes = [
+    {
+    path: '/',
+    redirect: '/home'
+}, {
+    path: '/home',
+    component: Home
+}, {
+        path: '/login',
+        component: Login
+}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ src/view/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-layout>
+    <the-header/>
+    <a-layout>
+      <the-sider/>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup>
+  import TheHeader from '../components/TheHeader/index.vue'
+  import TheSider from '../components/TheSider/index.vue'
+</script>
+
+<style scoped>
+.site-layout-background {
+  background: #fff;
+}
+</style>
